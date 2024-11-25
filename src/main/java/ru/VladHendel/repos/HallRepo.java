de115a51e2cfe476848b799f8fdc6b50208ccdb5
vlax6643 <vlax6643@mail.ru>
2024-11-26 00:58:18 +0300

    add img for init default tables

--- src/main/java/ru/VladHendel/repos/HallRepo.java
+++ src/main/java/ru/VladHendel/repos/HallRepo.java
@@ -1,6 +1,7 @@
 package ru.VladHendel.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.VladHendel.domain.Hall;
 
 public interface HallRepo extends JpaRepository<Hall, Long> {
